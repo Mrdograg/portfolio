66ec40b62dfcc11d5033bb5215b428343e526ab7
Mrdograg <dogra00amit@gmail.com>
2024-05-01 22:02:50 +0530

    Nvabar done

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
 import './navbar.css';
+import logo from '../../assets/logo.png';
+import {Link} from 'react-scroll';
+import contact from '../../assets/contact.png';
 
 const Navbar =()=>{
     return (
         <nav className="navbar">
-         <img src="" alt="" />
+         <img src={logo} alt="Logo" className="logo"/>
          <div className="desktopMenu">
+            <Link className="desktopMenuListItem">Home</Link>
+            <Link className="desktopMenuListItem">About</Link>
+            <Link className="desktopMenuListItem">TechStack</Link>
+            <Link className="desktopMenuListItem">Projects</Link>
 
          </div>
          <button className="desktopMenubtn">
-            <
-         </button>
+            <img src={contact} alt="" className="desktopMenuImg"/> Contact Me </button>         
         </nav>
     )
     }
