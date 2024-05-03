9797b21e6ff6545b4dfd631d2490552ded3e59a1
Mrdograg <dogra00amit@gmail.com>
2024-05-03 23:59:50 +0530

    UI completed

--- src/components/Contactme/contactme.js
+++ src/components/Contactme/contactme.js
@@ -5,13 +5,15 @@ import t from "../../assets/twitter.png"
  import g from "../../assets/Github.png"
  import gm from "../../assets/gmail.png"
  import c from "../../assets/call.png"
+import { Link } from "react-scroll";
 
 const Contactme = () =>{
     return (
    <section id="contactMe">
    <div className="headline"></div>
      <span className="contactMeTitle">Contact Me</span><br/>
-     <span className="contactMeDesc">Find all my contact details and profiles, including GitHub and LinkedIn, listed below. Don't hesitate to reach out via any platform.</span><br/>  
+     <span className="contactMeDesc">
+You can find my contact details, LinkedIn, GitHub and download my resume through the provided link. Feel free to connect with me via any platform—I'm accessible across all channels.</span><br/>  
      <span className="contactMeDesc1">Looking forward to connecting with you!</span><br/><br/>
      <div className="links">
         <a href="https://www.linkedin.com/in/mrdograg/"><img src={l} alt="linkedin" className="link"/></a>
@@ -20,6 +22,7 @@ const Contactme = () =>{
         <a href="mailto:dogra00amit@gmail.com"><img src={gm} alt="gmail" className="link"/></a>
         <a href="tel:+918588022643"><img src={c} alt="number" className="link"/></a><br/><br/>
         </div>
+        <a href="https://drive.google.com/file/d/1xmZsUyjsbTJTpgjDLFiN9NGAgNDtIoT2/view?usp=sharing"><button className="btn">Download Resume</button></a>
    </section>
     );
 }
