5f878ab6034d1049b71c950d98df703cb7ffaedd
Mrdograg <dogra00amit@gmail.com>
2024-05-05 00:08:56 +0530

    all set

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./intro.css";
-import bg from "../../assets/image.png";
+import bg from "../../assets/1.png";
 import { Link } from "react-scroll";
 import btnImg from "../../assets/hireme.png"
 
@@ -10,6 +10,7 @@ return (
     <div className="introContent">
       <span className="hello">Hello,</span>
       <span className="introText">I'm <span className="introName">Amit Dogra</span><br/>Software Developer</span>
+
       <p className="introPara">With expertise in frontend, backend, and databases, I thrive as a<br/> full-stack developer, particularly passionate about backend<br/> technologies. Delve into my portfolio below to witness my skills.<br/> Click "Get Hire" to employ my services or "Contact Me" to connect.<br/> Your visit is appreciated!</p>
       <Link><button className="btn" onClick={()=>{
             document.getElementById('contactMe').scrollIntoView({behavior: 'smooth'});
